d9b43e4aed558e04b454715859d7d9b520cdeb08
Mina <minaelhamy@gmail.com>
2026-04-27 16:26:39 +0300

    We have the real root cause now, and it’s in Servio, not the OS proxy.
    Servio storefront was crashing with:
    
    Class "App\Http\Controllers\front\Settings" not found
    from Servio/app/Http/Controllers/front/HomeController.php
    I patched it by adding the missing model import:
    
    Servio/app/Http/Controllers/front/HomeController.php

--- app/Http/Controllers/front/HomeController.php
+++ app/Http/Controllers/front/HomeController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Banner;
 use App\Models\Category;
+use App\Models\Settings;
 use App\Models\Service;
 use App\Models\Blog;
 use App\Models\AppSettings;
